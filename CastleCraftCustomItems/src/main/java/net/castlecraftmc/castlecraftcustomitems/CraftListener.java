55acc0105b461585650f64180c9dfdd19d5692e7
JackFlukinger <JackFlukinger@users.noreply.github.com>
2016-03-14 19:54:21 -0500

    Update CraftListener.java

--- CastleCraftCustomItems/src/main/java/net/castlecraftmc/castlecraftcustomitems/CraftListener.java
+++ CastleCraftCustomItems/src/main/java/net/castlecraftmc/castlecraftcustomitems/CraftListener.java
@@ -7,31 +7,31 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class CraftListener implements Listener {
-	@EventHandler
+	@EventHandler(ignoreCancelled=true)
 	public void CustomCraftEvent(CraftItemEvent e) {
 		//Make sure the event is not cancelled
-		if (!e.isCancelled()) {
-			if (e.getRecipe() instanceof ShapedRecipe) {
-				ItemStack item = e.getRecipe().getResult();
-				if (CastleCraftCustomItems.isItemInConfig(item)) {
-					ItemMeta meta = item.getItemMeta();
-					List<String> lore = CastleCraftCustomItems.getLoreFormat(item);
-					meta.setLore(lore);
-					meta.setDisplayName(CastleCraftCustomItems.getNameVariant(item));
-					item.setItemMeta(meta);
-					e.setCurrentItem(item);
-				}
+		if (e.getRecipe() instanceof ShapedRecipe) {
+			ItemStack item = e.getRecipe().getResult();
+			if (CastleCraftCustomItems.isItemInConfig(item)) {
+				ItemMeta meta = item.getItemMeta();
+				List<String> lore = CastleCraftCustomItems.getLore(item);
+				meta.setLore(lore);
+				meta.setDisplayName(CastleCraftCustomItems.getNameVariant(item));
+				meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+				item.setItemMeta(meta);
+				e.setCurrentItem(item);
 			}
 		}
 	}
 	
 	//Fixes items from before plugin installation
-	@EventHandler
+	@EventHandler(ignoreCancelled=true)
 	public void CustomCheckEvent(InventoryClickEvent e) {
 		ItemStack item = e.getCursor();
 		if (CastleCraftCustomItems.isItemInConfig(item)) {
@@ -41,9 +41,10 @@ public class CraftListener implements Listener {
 			} else {
 				meta = Bukkit.getItemFactory().getItemMeta(item.getType());
 			}
-			List<String> lore = CastleCraftCustomItems.getLoreFormat(item);
+			List<String> lore = CastleCraftCustomItems.getLore(item);
 			meta.setLore(lore);
 			meta.setDisplayName(CastleCraftCustomItems.getNameVariant(item));
+	        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
 			item.setItemMeta(meta);
 		}
 	}
